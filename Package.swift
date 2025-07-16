b39db35b9f52cb440d0a3232520dcad27db11024
zHElEARN <personal@zhelearn.com>
2025-07-16 19:12:58 +0800

    chore: update CSUSTKit dependency version to 1.0.20 to fix building problem

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         .package(url: "https://github.com/vapor/apns.git", from: "5.0.0"),
         .package(url: "https://github.com/MihaelIsaev/VaporCron.git", from: "2.6.0"),
-        .package(url: "https://github.com/zHElEARN/CSUSTKit.git", from: "1.0.19"),
+        .package(url: "https://github.com/zHElEARN/CSUSTKit.git", from: "1.0.20"),
     ],
     targets: [
         .executableTarget(
