826abfc507896dfb3e82057250713295d4a829e1
zHElEARN <personal@zhelearn.com>
2025-10-22 15:29:36 +0800

    feat(电量提醒): 优化查询失败时信息推送提示

--- Sources/CSUSTPlanetBackend/Utils/ElectricityJob.swift
+++ Sources/CSUSTPlanetBackend/Utils/ElectricityJob.swift
@@ -46,27 +46,41 @@ actor ElectricityJob {
             Task {
                 app.logger.info("开始执行电量查询任务，宿舍：\(electricityBinding.room)")
                 do {
-                    let electricity = try await ElectricityHelper.getInstance().getElectricity(
+                    let alert: APNSAlertNotification<EmptyPayload>
+                    if let electricity = try? await ElectricityHelper.getInstance().getElectricity(
                         campusName: electricityBinding.campus,
                         buildingName: electricityBinding.building,
                         room: electricityBinding.room
-                    )
-                    let alert = APNSAlertNotification(
-                        alert: .init(
-                            title: .raw("电量定时查询结果"),
-                            body: .raw("您的宿舍\(electricityBinding.room)当前电量为 \(electricity) 度")
-                        ),
-                        expiration: .immediately,
-                        priority: .immediately,
-                        topic: "com.zhelearn.CSUSTPlanet",
-                        badge: 0
-                    )
+                    ) {
+                        alert = APNSAlertNotification(
+                            alert: .init(
+                                title: .raw("电量定时查询结果"),
+                                body: .raw("您的宿舍\(electricityBinding.room)当前电量为 \(electricity) 度")
+                            ),
+                            expiration: .immediately,
+                            priority: .immediately,
+                            topic: "com.zhelearn.CSUSTPlanet",
+                            badge: 0
+                        )
+                        app.logger.info("电量查询通知发送成功，宿舍：\(electricityBinding.room)，电量：\(electricity) 度")
+                    } else {
+                        alert = APNSAlertNotification(
+                            alert: .init(
+                                title: .raw("电量定时查询结果"),
+                                body: .raw("无法查询到您的宿舍\(electricityBinding.room)的电量信息")
+                            ),
+                            expiration: .immediately,
+                            priority: .immediately,
+                            topic: "com.zhelearn.CSUSTPlanet",
+                            badge: 0
+                        )
+                        app.logger.warning("电量查询失败，宿舍：\(electricityBinding.room)")
+                    }
                     try await app.apns.client.sendAlertNotification(alert, deviceToken: electricityBinding.deviceToken)
-                    app.logger.info("电量查询通知发送成功，宿舍：\(electricityBinding.room)，电量：\(electricity) 度")
                 } catch let apnsError as APNSError {
                     switch apnsError.reason {
                     case .badDeviceToken, .unregistered, .deviceTokenNotForTopic:
-                        app.logger.error("设备令牌无效，取消电量定时任务，宿舍：\(electricityBinding.room)")
+                        app.logger.warning("设备令牌无效，取消电量定时任务，宿舍：\(electricityBinding.room)")
                         try? await ElectricityJob.shared.cancelJob(app: app, electricityBinding: electricityBinding)
                         try? await electricityBinding.delete(on: app.db)
                     default:
@@ -78,6 +92,6 @@ actor ElectricityJob {
             }
         }
         jobs[id] = job
-        app.logger.info("配置调度电量查询任务，宿舍：\(electricityBinding.room)，时间：\(electricityBinding.scheduleHour):\(electricityBinding.scheduleMinute)")
+        app.logger.info("调度电量查询任务，宿舍：\(electricityBinding.room)，时间：\(electricityBinding.scheduleHour):\(electricityBinding.scheduleMinute)")
     }
 }
